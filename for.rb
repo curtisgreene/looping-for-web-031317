9c58a7a1cd2340b9ab45ea79cda87ecbe3009a48
Curtis Greene <curtis.j.w.greene@gmail.com>
2017-03-03 19:44:18 +0000

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,7 @@
-
 def using_for
-	checklist = 1..10
+	checklist = 1..10 #sets the range
 	#your code here
+	for item in checklist #each individual element in the range (1-10)
+		puts "Wingardium Leviosa"
+	end
 end
-
-
